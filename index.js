093557adbdf295fb36c8fc89ca41bda675e990ca
Lisa Chan <xlisachan@gmail.com>
2018-02-15 17:51:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+funciton destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kitten.shift();
+}
+
+function appendKitten(name){
+  var newKittens = kittens.push(name);
+  return newKittens;
+}
+
+function prependKitten(name){
+  var newKittens = kittens.unshift(name);
+  return newKittens;
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens;
+}
+
+function removeFirstKitten(){
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
